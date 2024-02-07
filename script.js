e48369d6d613f5702dd442e0e857d36d131726a1
Jubayer-K <jubayerkhan21@gmail.com>
2024-02-08 03:44:17 +0600

    completed problem 5

--- script.js
+++ script.js
@@ -27,18 +27,67 @@ function checkName(name) {
 }
 
 function deleteInvalids(array) {
-  if (Array.isArray(array)) {
+  if (Array.isArray(array) === true) {
     const numArray = [];
     for (const item of array) {
-      if (typeof item === 'number' && !isNaN(item) && !null && !undefined) {
+      if (typeof item === "number" && !isNaN(item) && !null && !undefined) {
         numArray.push(item);
       }
     }
     return numArray;
-  } 
-  else {
+  } else {
     return "“Invalid Array” Enter a valid array";
   }
 }
 
-console.log(deleteInvalids([1,2,3,-4 ]));
+function password(obj) {
+  if (typeof obj === "object" && obj !== null && obj !== undefined) {
+    if (
+      obj.hasOwnProperty("name") &&
+      obj.hasOwnProperty("birthYear") &&
+      obj.hasOwnProperty("siteName")
+    ) {
+      if (
+        typeof obj.birthYear === "number" &&
+        obj.birthYear >= 1000 &&
+        obj.birthYear <= 9999
+      ) {
+        let upObj = obj.siteName + "#" + obj.name + "@" + obj.birthYear;
+        return upObj.charAt(0).toUpperCase() + upObj.slice(1);
+      } else {
+        return "invalid";
+      }
+    } else {
+      return "invalid";
+    }
+  } else {
+    return "invalid";
+  }
+}
+
+function monthlySavings(arr, livingCost) {
+  if (Array.isArray(arr) && typeof livingCost === "number") {
+    let salary = 0;
+    for (const item of arr) {
+      if (typeof item === "number") {
+        if (item >= 3000) {
+          salary = salary + item - item * 0.2;
+        } else {
+          salary = salary + item;
+        }
+      } else {
+        return "invalid input";
+      }
+    }
+    const savings = salary - livingCost;
+    if (savings < 0) {
+      return "earn more";
+    } else {
+      return savings;
+    }
+  } else {
+    return "invalid input";
+  }
+}
+
+console.log(monthlySavings('dajfh'));
